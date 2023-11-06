559840fe2c97cef4c64a29b7b64ab06241319f21
Manuel Zimmermann <zimmermannm@udesa.edu.ar>
2023-11-06 10:42:47 -0300

    login y register cocinandose

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,27 +1,120 @@
-import React, {Component} from 'react';
-import { View, Text, TextInput, TouchableOpacity} from 'react-native';
-class Login extends Component{ 
-    constructor(){
-        super()
-        this.state={
+import React, { Component } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { auth } from '../firebase/config';
 
-        }
-    }
-    componentDidMount(){ console.log('Fede')
-    }
-    render(){
-        console.log('vvd');
-        return (
-            <>
-            <Text> login </Text>
-                </>
-        )
-    }
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      pass: '',
+      errors: '',
+    };
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        this.props.navigation.navigate('HomeMenu');
+      }
+    });
+  }
+
+  loginUser(email, pass) {
+    auth
+      .signInWithEmailAndPassword(email, pass)
+      .then(res => {
+        this.props.navigation.navigate('HomeMenu');
+      })
+      .then(this.setState({
+        email: '',
+        pass: '',
+      }))
+      .catch(error => this.setState({ errors: error }));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titulo}>LOGIN</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Email"
+          keyboardType="email-address"
+          onChangeText={text => this.setState({ errors: '', email: text })}
+          value={this.state.email}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Password"
+          keyboardType="default"
+          secureTextEntry={true}
+          onChangeText={text => this.setState({ errors: '', pass: text })}
+          value={this.state.pass}
+        />
+        {this.state.errors == '' ? (
+          <TouchableOpacity onPress={() => this.loginUser(this.state.email, this.state.pass)}>
+            <Text style={styles.button}> Ingresar</Text>
+          </TouchableOpacity>
+        ) : (
+          <Text style={styles.notificacion}>{this.state.errors.message}</Text>
+        )}
+        <TouchableOpacity onPress={() => this.props.navigation.navigate('Register')}>
+          <Text style={styles.register}>Ir a registro</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
-ama[m
-]
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000', // Fondo negro
+  },
+  titulo: {
+    color: '#D32F2F', // Texto rojo
+    fontWeight: 'bold',
+    fontSize: 35,
+    marginBottom: 20,
+  },
+  textInput: {
+    color: '#D32F2F',
+    borderBottomWidth: 2,
+    borderColor: '#D32F2F',
+    borderRadius: 4,
+    marginTop: 10,
+    fontSize: 18,
+    width: '80%',
+  },
+  button: {
+    marginTop: 20,
+    height: 32,
+    color: 'black',
+    backgroundColor: '#D32F2F',
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+    padding: 6,
+    width: '100%',
+  },
+  notificacion: {
+    color: '#D32F2F',
+    marginTop: 20,
+    fontSize: 20,
+  },
+  register: {
+    color: '#D32F2F',
+    marginTop: 20,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
 
 export default Login;
+
 
 // mismo procedimiento para todos los screens.
 
--- src/screens/register.js
+++ src/screens/register.js
@@ -1,16 +1,167 @@
-import React, {Component} from 'react';
-class Register extends Component{ 
-    constructor(){
-        super()
-        this.state={
+import React, { Component } from 'react';
+import { auth, db } from '../firebase/config';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
 
-        }
-    }
-    render(){
-        return (
-            <></>
-        )
-    }
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      pass: '',
+      nombre_usuario: '',
+      mini_bio: '',
+      errors: '',
+      foto_perfil: '',
+      showCamera: false,
+    };
+  }
+  registerUser(email, pass, nombre_usuario, mini_bio, foto_perfil) {
+    auth
+      .createUserWithEmailAndPassword(email, pass)
+      .then((res) => {
+        db.collection('usuarios')
+          .add({
+            owner: email,
+            nombre_usuario: nombre_usuario,
+            mini_bio: mini_bio,
+            foto_perfil: foto_perfil,
+            createdAt: Date.now(),
+          })
+          .then(() => {
+            this.setState({
+              email: '',
+              pass: '',
+              userName: '',
+              bio: '',
+              errors: '',
+              showCamera: false,
+            });
+            this.props.navigation.navigate('Login');
+          })
+          .catch((error) => console.log(error));
+      })
+      .catch((error) => this.setState({ errors: error }));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titulo}>REGISTRO</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Email"
+          keyboardType="email-address"
+          onChangeText={(text) => this.setState({ errors: '', email: text })}
+          value={this.state.email}
+        />
+        <TextInput
+         style={styles.textInput}
+          placeholder="Password"
+          keyboardType="default"
+          secureTextEntry={true}
+          onChangeText={(text) => this.setState({ errors: '', pass: text })}
+          value={this.state.pass}
+        />
+        <TextInput
+         style={styles.textInput}
+          placeholder="User name"
+          keyboardType="default"
+          onChangeText={(text) => this.setState({ errors: '', nombre_usuario: text })}
+          value={this.state.nombre_usuario}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Mini Bio"
+          keyboardType="default"
+          onChangeText={(text) => this.setState({ errors: '', mini_bio: text })}
+          value={this.state.mini_bio}
+        />
+        { this.state.showCamera ?
+                        <View style={{width: '100vw', heigth: '100vh'}}>
+                            <MyCamera onImageUpload={url => this.onImageUpload(url)}/> 
+                        </View> 
+                        :
+                        <TouchableOpacity onPress={()=> this.setState({showCamera:true})}>
+                            <Text style={styles.textInpu} > Subir foto de perfil</Text>
+                        </TouchableOpacity>
+                    }
+
+                    {
+                    this.state.email == '' || this.state.pass == '' || this.state.nombre_usuario == ''  || this.state.foto_perfil == '' ?
+                    <Text > Completar los campos</Text> 
+                    :
+                     
+        
+                <TouchableOpacity
+                style={styles.button}
+                onPress={() =>
+                this.registerUser(
+                    this.state.email,
+                    this.state.pass,
+                    this.state.nombre_usuario,
+                    this.state.mini_bio,
+                    this.state.foto_perfil
+                    )
+                }
+                >
+                <Text>REGISTRARME</Text>
+                </TouchableOpacity>
+                 }
+        <Text style={styles.errorText}>{this.state.errors.message}</Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white', // Fondo blanco
+  },
+  titulo: {
+    color: '#D32F2F', // Texto rojo
+    fontWeight: 'bold',
+    fontSize: 35,
+    marginBottom: 20,
+  },
+  textInput: {
+    color: '#D32F2F',
+    borderBottomWidth: 2,
+    borderColor: '#D32F2F',
+    borderRadius: 4,
+    marginTop: 10,
+    fontSize: 18,
+    width: '80%',
+  },
+  textInpu: {
+    color: '#D32F2F',
+    borderBottomWidth: 2,
+    borderColor: '#D32F2F',
+    borderRadius: 4,
+    marginTop: 10,
+    fontSize: 18,
+    width: '100%',
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: '#D32F2F',
+    padding: 10,
+    borderRadius: 5,
+  },
+  errorText: {
+    color: '#D32F2F',
+    marginTop: 20,
+    fontSize: 18,
+  },
+});
 
 export default Register;
