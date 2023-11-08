37e7d9b979c3e2d00a6cfd2bc2b7154c773692e4
Manuel Zimmermann <zimmermannm@udesa.edu.ar>
2023-11-08 18:51:51 -0300

    info posts

---
+++ src/components/unPost.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+
+class unPost extends Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text >unPost</Text>
+        
+      </View>
+    );
+  }
+}
+
+
+export default unPost;
+
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-
+import { db } from '../firebase/config';
 
 class Home extends Component {
   constructor() {
     super();
     this.state = {
+      posts: []
+
       
     };
   }
+  componentDidMount(){
+    db.collection('posts').orderBy('fecha', 'desc').onSnapshot(
+      documentos => {
+        let posts = [];
+        documentos.forEach( doc => {
+            posts.push({
+                id: doc.id,
+                data: doc.data()
+            })
+            this.setState({
+                posts: posts
+            })
+    })
+      })
+          
+  }
 
-  render() {
+  render() { 
+    console.log(this.state.posts)
     return (
       <View>
         <Text >Home</Text>
