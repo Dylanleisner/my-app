5f0722d70c22ba8b781a09a5f3f59656a5b69dfe
Manuel Zimmermann <zimmermannm@udesa.edu.ar>
2023-11-15 11:32:05 -0300

    profile cocinandose

--- src/screens/profile.js
+++ src/screens/profile.js
@@ -20,23 +20,23 @@ class Profile extends Component {
   
 
   // ESTO ROMPE --> LO DEJE ACA PARA PODER ANALISARLO
-  // componentDidMount(){
-  //   const profileEmail = this.props.route.params.email;
+  componentDidMount(){
+    const profileEmail = this.props.route.params.email;
 
-  //   db.collection('posts').where('owner', '==', profileEmail).onSnapshot( 
-  //       docs => {
-  //           let posts = [];
-  //           docs.forEach( doc => {
-  //               posts.push({
-  //                   id: doc.id,
-  //                   data: doc.data()
-  //               })
-  //               this.setState({
-  //                   posts: posts
-  //               })
-  //           }) 
-  //       }
-  //   )}
+    db.collection('posts').where('owner', '==', profileEmail).onSnapshot( 
+        docs => {
+            let posts = [];
+            docs.forEach( doc => {
+                posts.push({
+                    id: doc.id,
+                    data: doc.data()
+                })
+                this.setState({
+                    posts: posts
+                })
+            }) 
+        }
+    )}
 
 
 
