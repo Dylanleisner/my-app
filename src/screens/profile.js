deb2c6747e6f1696e22d159e873e0d54b3e7af2b
tomaslegarre <tlegarrematera@udesa.edu.ar>
2023-11-15 09:10:28 -0300

    arrancando profile

--- src/screens/profile.js
+++ src/screens/profile.js
@@ -21,6 +21,27 @@ class Profile extends Component {
   }
   
 
+  // ESTO ROMPE --> LO DEJE ACA PARA PODER ANALISARLO
+  // componentDidMount(){
+  //   const profileEmail = this.props.route.params.email;
+
+  //   db.collection('posts').where('owner', '==', profileEmail).onSnapshot( 
+  //       docs => {
+  //           let posts = [];
+  //           docs.forEach( doc => {
+  //               posts.push({
+  //                   id: doc.id,
+  //                   data: doc.data()
+  //               })
+  //               this.setState({
+  //                   posts: posts
+  //               })
+  //           }) 
+  //       }
+  //   )}
+
+
+
   render() {
     return (
       <View>
